e9306e653f163e7b1ccb0ad81663aef6b4953ae5
Aashu Kumar <ashu9472.as@gmail.com>
2025-10-21 20:24:51 +0530

    User Schema Added

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+    
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    profilePicture: {
+        type: String,
+        default: ''
+    }
+
+}, {
+    timestamps: true
+})
+
+// Hash password before saving
+userSchema.pre('save', async (next) => {
+    
+    if (this.isModified('password')) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async (candidatePassword) => {
+    return await bcrypt.compare(candidatePassword, this.password)
+}
+
+const User = mongoose.model('User', userSchema)
+export default User
